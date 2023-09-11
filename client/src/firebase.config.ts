1e76b3d00550ae68b6e8c0aa35a18127317146ef
Yashvardhan Arora <yash22arora@gmail.com>
2023-09-12 01:21:12 +0530

    Add firebase bucket id as env var
    Signed-off-by: Yashvardhan Arora <yash22arora@gmail.com>

--- client/src/firebase.config.ts
+++ client/src/firebase.config.ts
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: "servdomain-b7fea.appspot.com",
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: "G-SFZPYK332J",
