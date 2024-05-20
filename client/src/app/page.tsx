2bdbb8cc027a48f8c1b2b909feb7788f8dbf4c84
Yashvardhan Arora <yash22arora@gmail.com>
2024-05-21 03:02:10 +0530

    Add accordian on landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,6 +4,12 @@ import Pricing from "@/components/Landing/Pricing/Pricing";
 import Waitlist from "@/components/Landing/Waitlist";
 import Callout from "@/components/common/Callout";
 import Link from "next/link";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function Landing() {
   return (
@@ -12,6 +18,62 @@ export default function Landing() {
       <CheckForm className="mt-12" />
       <Waitlist />
       <Pricing />
+      <Callout className="mb-12 w-full max-w-xxl">
+        <Accordion className=" " type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger className="text-2xl font-semibold">
+              Why do I need this 🤔 ?
+            </AccordionTrigger>
+            <AccordionContent className="text-base">
+              There are a plenty of reasons why you need this. Here are a few:
+              <ul className="list-decimal list-inside gap-6 flex flex-col my-4">
+                <li>
+                  If you are a student or young tech enthusiast with less budget
+                  but dont wanna restrict yourself to free subdomains like{" "}
+                  <em className="underline underline-offset-4">
+                    your-project-name-and-some-extra-gibberish-h8735d.versail.com.
+                  </em>{" "}
+                  (Yuck! Will you put that on your résume ??)
+                </li>
+                <li>
+                  If you have a VM running on cloud, hosting your application
+                  and you are done accessing it through its IP address. (I mean,
+                  you can keep using{" "}
+                  <em className="underline underline-offset-4">
+                    11.147.208.49/api/hello
+                  </em>{" "}
+                  but{" "}
+                  <em className="underline underline-offset-4 text-purple-400">
+                    cool-api.servatom.xyz/api/hello
+                  </em>{" "}
+                  is so much cooler and easy to remember!)
+                </li>
+                <li>
+                  If you only need a good-looking domain for a few weeks and
+                  dont want to pay hefty annual amounts for it.
+                </li>
+                <li>
+                  If you really love a domain, and wish if you could own at
+                  least a part of it, if not all?
+                </li>
+                <li>
+                  If you are new to networking and DNS and want to experiment
+                  with CNAME / A / TXT records.
+                </li>
+              </ul>
+              <p>.</p>
+              <p>.</p>
+              <p>.</p>
+              <p>.</p>
+              <p>.</p>
+              <p className="mt-8">
+                The list goes on and on. But you get the point, right?
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </Callout>
+
       <div className="flex flex-row items-center gap-3 mb-8 opacity-80 text-sm underline underline-offset-4">
         <Link href={"/privacy"} className="text-center ">
           Privacy Policy
